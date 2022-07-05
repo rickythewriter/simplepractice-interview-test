f198b0a8304205d737ee738f73f70531417cb02b
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-05 15:28:08 -0700

    feature - draft api/appointments endpoint with filtered routes functionality

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,8 +1,35 @@
 class Api::AppointmentsController < ApplicationController
   def index
-    # TODO: return all values
-    @appointments = Appointment.all
-    # TODO: return filtered values
+
+    # Initialize @appointment instance
+    if defined?(params[:past]) # Initialize only past or only future appointments
+
+      # Declare conditions
+      showOnlyPastAppointments = params[:past] == 1
+      showOnlyFutureAppointments = params[:past] == 0
+
+      # Note: appointments are ordered by start_time, from closest to furthest
+      if showOnlyPastAppointments
+        @appointments = Appointment.where("start_time < ?", DateTime.now).order(start_time: :asc)
+      elsif showOnlyFutureAppointments
+        @appointments = Appointment.where("start_time > ?", DateTime.now).order(start_time: :asc)
+      end
+
+    else # Initialize all appointments
+      @appointments = Appointment.all
+    end
+    
+    # Paginate, if applicable
+    if defined?(params[:length] && params[:page])
+      length = params[:length]
+      idx_start = (params[:page] - 0) * length #idx_start is the offset from the beginning
+
+      # only include length number of appointments, starting at index of offset
+      @appointments = @appointments[idx_start, length]
+    end
+    
+    @appointments
+
     head :ok
   end
 
