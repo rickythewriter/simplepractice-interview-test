c9ac05330d97f2d2bad98a2d13c87202aaaff1d7
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-05 16:49:18 -0700

    feature - draft api/doctors endpoint

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -1,6 +1,20 @@
 class Api::DoctorsController < ApplicationController
     def index
-        # TODO: return all doctors with no appointments
+
+        # Initialize doctors instance
+        @doctors = Doctor.all
+
+        # Delete doctors with appointments
+        @doctors.each do |doctor|
+            hasAppointment = Appointment.where(doctor_id: doctor.id).length != 0
+            if hasAppointment
+                @doctors.delete(doctor)
+            end
+        end
+
+        # Return doctors without appointments
+        @doctors
+
         head :ok
     end
 end
