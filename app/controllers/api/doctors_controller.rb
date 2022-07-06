1b718e1e946bad1f87a114a0ea1a04ce4f7d7551
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-06 14:05:50 -0700

    bug fix - move helper function to doctor model; change code to return doctors with no appointments on every call instead of all doctors on the first call

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -1,20 +1,9 @@
 class Api::DoctorsController < ApplicationController
     def index
 
-        # Initialize doctors instance
-        @doctors = Doctor.all
-
-        # Delete doctors with appointments
-        @doctors.each do |doctor|
-            hasAppointment = Appointment.where(doctor_id: doctor.id).length != 0
-            if hasAppointment
-                @doctors.delete(doctor)
-            end
-        end
-
-        # Return doctors without appointments
-        # head :ok
-        render json: @doctors, status: 200
+        doctors = Doctor.doctors_without_appointments
         
+        render json: doctors, status: 200        
+
     end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,22 @@
 class Doctor < ApplicationRecord
   has_many :patients
   has_many :appointments
+
+  def self.doctors_without_appointments
+
+    # Initialize doctors variable
+    doctors = []
+
+    # Delete doctors with appointments
+    Doctor.all.each do |doctor|
+        hasAppointment = Appointment.where(doctor_id: doctor.id).length != 0
+        if !hasAppointment
+            doctors.push(doctor)
+        end
+    end
+
+    return doctors
+
+  end
+
 end
