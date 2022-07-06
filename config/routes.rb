ec190b304d7293a1abba712c9faa7f01a954eb4e
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-06 14:04:47 -0700

    bug fix - make only get route available for doctor api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :appointments, only: [:index]
-    resources :doctors
+    get '/doctors', to: 'doctors#index'
   end
 end
