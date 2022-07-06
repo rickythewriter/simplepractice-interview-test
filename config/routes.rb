52da265ca2466bb33186e5b93d1b4b31fef3226c
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-05 22:26:50 -0700

    feature - add post route for api/appointments

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :appointments, only: [:index]
+    post '/appointments', to: 'appointments#create'
   end
 end
