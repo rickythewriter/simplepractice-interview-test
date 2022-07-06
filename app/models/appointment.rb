651e4248ace06226c86d7fdce3ff227576de71db
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-06 09:50:26 -0700

    docs - add to-do comments in Appointment class

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,15 @@
 class Appointment < ApplicationRecord
+
+# TODO: Validate data types
+# {
+#   patient: { name: <string> },
+#   doctor: { id: <int> },
+#   start_time: <iso8604>,
+#   duration_in_minutes: <int>
+# }
+
+#Note: Class methods start with self.
+
   belongs_to :doctor
   belongs_to :patient
 end
