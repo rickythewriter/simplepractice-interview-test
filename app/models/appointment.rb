b0a0e5872f4f32f40d606af1242ab6d4c359472f
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-11 16:37:38 -0700

    refactor - Appointment Model - make helper functions private

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -10,14 +10,6 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
 
-  def self.past_appointments
-    self.where("start_time < ?", DateTime.now).order(start_time: :desc)
-  end
-
-  def self.future_appointments
-    self.where("start_time > ?", DateTime.now).order(start_time: :asc)
-  end
-
   def self.future_or_past_appointments(past)
     if past == "1"
       self.past_appointments
@@ -44,6 +36,16 @@ class Appointment < ApplicationRecord
     else
       return nil
     end
+  end
+
+  private
+
+  def self.past_appointments
+    self.where("start_time < ?", DateTime.now).order(start_time: :desc)
+  end
+
+  def self.future_appointments
+    self.where("start_time > ?", DateTime.now).order(start_time: :asc)
   end
 
 end
