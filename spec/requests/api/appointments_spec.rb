8dde9522de3382f9a1ed7814ce32c29c5b95e6c3
Ricky Thang <ricky.writer.dev@gmail.com>
2022-07-11 22:59:31 -0700

    test - api/appointments - write pagination tests

--- spec/requests/api/appointments_spec.rb
+++ spec/requests/api/appointments_spec.rb
@@ -47,4 +47,93 @@ RSpec.describe "/api/appointments", type: :request do
         end
     end
 
+    describe "GET /api/appointments" do
+        it "returns paginated appointments, starting at `page`; use page size of `length`" do
+
+            # Parameter to be tested
+            num_of_entries = 8
+
+            get "/api/appointments", params:{
+                length: num_of_entries,
+                page: 1
+            }
+
+            appointments_on_page = JSON.parse(response.body)
+            puts "There are " + appointments_on_page.length.to_s + " appointments on the page."
+            expect(appointments_on_page.length).to eql num_of_entries
+        end
+
+        it "returns remaining paginated appointments, on the last page" do
+
+            get "/api/appointments", params:{
+                length: 4,
+                page: 3
+            }
+            appointments_on_page = JSON.parse(response.body) #appointments 9 and 10 of 10
+            expect(appointments_on_page.length).to eql 2
+        end
+
+        it "returns paginated appointments, in the correct order" do
+
+            # Parameters to be tested
+            length = 4
+            page = 2 # must be before final page, if final page not full-length
+
+            get "/api/appointments", params:{
+                length: length,
+                page: page
+            }
+
+            appointments_on_page = JSON.parse(response.body)
+
+            # Initialize all appointments for comparison with appointments on page
+            appointments_comprehensive = Appointment.all
+
+            # Initialize offset for index
+            offset_for_comprehensive_idx = length * (page - 1)
+
+            # Check if sequence of appointments in correct order
+            for idx in 0...length do
+
+                id_appointment_on_page = appointments_on_page[idx]["id"]
+                id_appointment_comprehensive = appointments_comprehensive[idx + offset_for_comprehensive_idx]["id"]
+
+                # puts id_appointment_on_page.to_s + " == " + id_appointment_comprehensive.to_s + ": " + (id_appointment_on_page == id_appointment_comprehensive).to_s
+
+                expect(id_appointment_on_page).to eql (id_appointment_comprehensive)
+            end
+
+            # Check if correct number of entries returned on page
+            expect(appointments_on_page.length).to eql length
+        end
+
+        it "returns no appointments, when `page` is 0" do
+            get "/api/appointments", params:{
+                length: 4,
+                page: 0
+            }
+            appointments_on_page = JSON.parse(response.body)
+            expect(appointments_on_page.length).to eql 0
+        end
+
+        it "returns no appointments, when `page` is less than 0" do
+            get "/api/appointments", params:{
+                length: 4,
+                page: -1
+            }
+            appointments_on_page = JSON.parse(response.body)
+            expect(appointments_on_page.length).to eql 0
+        end
+
+        it "returns no appointments, when `page` is out of bounds" do
+            get "/api/appointments", params:{
+                length: 5,
+                page: 3
+            }
+            appointments_on_page = JSON.parse(response.body)
+            expect(appointments_on_page.length).to eql 0
+        end
+
+    end
+    
 end
